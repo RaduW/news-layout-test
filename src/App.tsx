237fb2f6d0931cb2b8912fb9aa646c91d0c9da6e
Radu Woinaroski <radu@sentry.io>
2021-07-01 16:44:08 +0200

    add button support

--- src/App.tsx
+++ src/App.tsx
@@ -30,10 +30,15 @@ function initialAppState(): AppState {
 export function App() {
     const [state, setState]: [AppState, (state: AppState) => void] = useState(initialAppState());
     const setColumns = (columns: number) => setState({...state, columns})
+    const setElementType=(idx:number, type: ElementType) => {
+        let elements = [...state.elements]
+        elements[idx] = { ...elements[idx], type}
+        setState({...state, elements})
+    }
     return (
         <div className="App">
             <Toolbar columns={state.columns} setColumns={setColumns}/>
-            <ControlPanel elements={state.elements}/>
+            <ControlPanel elements={state.elements} setElementType={setElementType}/>
             <GridPanel {...state}/>
         </div>
     )
@@ -41,11 +46,16 @@ export function App() {
 
 interface ControlPanelProps {
     elements: DisplayElement[]
+    setElementType: (idx: number, type: ElementType)=>void
 }
 
 function ControlPanel(props: ControlPanelProps) {
-    const elements = props.elements.map((elm) =>
-        <ElementController elm={elm}/>
+    const setElement=(idx:number)=>(type: ElementType)=>{
+        props.setElementType(idx, type)
+        //props.elements[idx] = {...props.elements[idx], type}
+    }
+    const elements = props.elements.map((elm,idx) =>
+        <ElementController key={idx} elm={elm} setType={setElement(idx)}/>
     )
     return (<div className="ControlPanel">
         {elements}
@@ -108,6 +118,7 @@ function ColumnsButton(props: ColumnsButtonProps) {
 
 interface ElementControllerProps {
     elm: DisplayElement
+    setType: ( elementType: ElementType)=>void
 }
 
 function ElementController(props: ElementControllerProps) {
@@ -118,12 +129,12 @@ function ElementController(props: ElementControllerProps) {
     const ref_active = props.elm.type === 'references' ? "active" : ""
     return (
         <div className='button-container'>
-            <span className="item-name">{props.elm.id}</span>
-            <span className={`small-button ${art_active} article`}>art</span>
-            <span className={`small-button ${sp_active} sports`}>sp</span>
-            <span className={`small-button ${imp_active} important`}>imp</span>
-            <span className={`small-button ${ad_active} ad`}>ad</span>
-            <span className={`small-button ${ref_active} references`}>ref</span>
+            <span className="item-name" onClick={(_)=>props.setType("article")}>{props.elm.id}</span>
+            <span className={`small-button ${art_active} article`} onClick={(_)=>props.setType("article")}>art</span>
+            <span className={`small-button ${sp_active} sports`} onClick={(_)=>props.setType("sports")}>sp</span>
+            <span className={`small-button ${imp_active} important`} onClick={(_)=>props.setType("important")}>imp</span>
+            <span className={`small-button ${ad_active} ad`} onClick={(_)=>props.setType("ad")}>ad</span>
+            <span className={`small-button ${ref_active} references`} onClick={(_)=>props.setType("references")}>ref</span>
         </div>
     )
 }
